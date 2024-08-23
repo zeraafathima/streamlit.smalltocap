2a940ad670a7df5f25a6ad8a865cc3205c7fb165
zeraafathima <fathimadilawar09@gmail.com>
2024-08-23 07:20:23 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 st.header('small to capital application')
+st.image('images.jfif')
 st.write('This is a application to change lowercase to uppercase letters')
 y=st.text_input(label='enter the sentence')
 x=st.button('change')
