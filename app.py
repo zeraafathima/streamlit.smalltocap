b59ab65ffae183eeced2602782035830f1263f63
zeraafathima <fathimadilawar09@gmail.com>
2024-08-23 07:08:51 +0000

    1

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st 
+st.header('small to capital application')
+st.write('This is a application to change lowercase to uppercase letters')
+y=st.text_input(label='enter the sentence')
+x=st.button('change')
+if x:
+    y=y.upper()
+    st.subheader(y)
